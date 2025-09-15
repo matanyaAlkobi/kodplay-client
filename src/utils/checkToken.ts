ac1ea24812eadda014fff7b7e0d8bee7d24621e5
elazar matan <elazarmata8@gmail.com>
2025-09-15 11:06:32 +0300

    change port

--- src/utils/checkToken.ts
+++ src/utils/checkToken.ts
@@ -1,7 +1,7 @@
 export default async function checkIfTokenValid(){
     const token = localStorage.getItem('token_kodePlay')
     console.log(`token: ${token}`)
-    const res = await fetch('http://localhost:3000/users/checkToken',{
+    const res = await fetch('http://localhost:4545/users/checkToken',{
          method: "GET",
     headers: {
         "Authorization": `Bearer ${token}`
