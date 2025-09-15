622106b1e66d3331c9e6eab3e29ab332e86c5198
elazar matan <elazarmata8@gmail.com>
2025-09-15 09:48:17 +0300

    feat: Add entry and login pages, update routing
    This commit introduces two new pages: an entry page and a login page. The routing configuration has been updated to include these pages, and removes react-router-dom dependency.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { ImagePickerPage } from "../pages/ImagePickerPage.tsx";
 import Layout from "../components/application-layout/Layout.tsx";
+import Entry from "../pages/entry.tsx";
+import Login from "../pages/login.tsx";
 
 export default function Router() {
   return (
     <BrowserRouter> 
       <Routes>
+            <Route path="/" element={<Entry/>}/>
+            <Route path="/login" element={<Login/>}/>
         <Route element={<Layout />}>
           <Route path="/" element={<ImagePickerPage />} />
         </Route>
