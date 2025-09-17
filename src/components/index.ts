e723b603ea4bd6f8ae371ee78ab057be6163eab0
Oryan Menachem <om3160204@gmail.com>
2025-09-17 12:03:39 +0300

    style

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import detectFacialExpression from "./FaceDetection/services";
 import FaceDetection from "./FaceDetection/FaceDetection";
 import Layout from "./application-layout/Layout";
 import { UploadImage, TakePicture } from "./application-layout/UploadImage";
+import Playlist from "./Playlist";
 
-export { detectFacialExpression, FaceDetection, Layout, UploadImage, TakePicture };
+export { detectFacialExpression, FaceDetection, Layout, UploadImage, TakePicture, Playlist };
